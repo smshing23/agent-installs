957679e31e4c3e993abbf28c89e909287d66e129
smshing23 <45262510+smshing23@users.noreply.github.com>
2025-02-17 13:42:48 -0500

    Update cloud_radial_agent_installer.sh

--- cloud_radial_agent_installer.sh
+++ cloud_radial_agent_installer.sh
@@ -11,11 +11,11 @@ COMPANY_ID="$1"
 if [[ $(uname -p) == 'arm' ]]; then
     echo "Installing for Apple M1"
     sudo curl "https://cloudradialagent.blob.core.windows.net/macagent/cloudradial.mac.agent-arm64.pkg" --output "/tmp/cloudradial.mac.agent-arm64.pkg"
-    sudo installer -pkg "/tmp/cloudradial.mac.agent-arm64.pkg" -target /
-elif
+    sudo installer -pkg "/tmp/cloudradial.mac.agent-arm64.pkg" -target "/"
+else
     echo "Installing for Intel"
     curl "https://cloudradialagent.blob.core.windows.net/macagent/cloudradial.mac.agent-x64.pkg" --output "/tmp/cloudradial.mac.agent-x64.pkg"
-    sudo installer -pkg "/tmp/cloudradial.mac.agent-x64.pkg" -target /
+    sudo installer -pkg "/tmp/cloudradial.mac.agent-x64.pkg" -target "/"
 fi
 # Update plist with correct settings for partner and company
 plutil -remove ProgramArguments "/Library/LaunchDaemons/com.cloudradial.mac.agent.plist" > /dev/null
